2429e9cad3da6f3b5a3768948c966dd04c98d1ac
Piotr Roszkowski <piotroszko2@gmail.com>
2026-02-03 15:07:17 +0100

    fix: build for api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // eslint-disable-next-line turbo/no-undeclared-env-vars
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
